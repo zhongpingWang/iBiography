5d33a5c5e49aad5f862b534b7963907c33d5799d
wangzp-a <wangzp-a@grandsoft.com.cn>
2018-12-02 22:28:26 +0800

    init server

---
+++ server/index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+
+app.get('/page/index**', (req, res) => res.sendFile(__dirname + "\/tpl\/index.html"));
+
+
+
+app.listen(80, () => console.log('Example app listening on port 80!'))
